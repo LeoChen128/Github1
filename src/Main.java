cf447838360678383cd6e39d2712e6d375c68f62
BT_2S14_34 <leo1281515@gmail.com>
2024-09-16 10:12:28 -0400

    Added a print statement about my favorite food.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Leo Chen");
-        System.out.println("My favorite food is: mangosteen.");
+        System.out.println("My favorite food is mangosteen.");
     }
 }
